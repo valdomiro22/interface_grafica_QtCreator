0a820c61b8425d9af63c720a977a2c74bd5218ab
Valdomiro Santos <valdomiro.jose@outlook.com>
2021-08-06 13:36:47 -0300

    Caixas de menssagem

--- aula04/mainwindow.cpp
+++ aula04/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>  // Necessaria para as caixas de mensagens
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +12,27 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    // Caixa informativa
+//    QMessageBox::about(this, "CFB Cursos", "Curso de Qt Creator");
+
+    // Caixa sobre o Qt creator
+//    QMessageBox::aboutQt(this, "Curso de Qt Creator");
+
+    // Caixa alerta
+//    QMessageBox::critical(this, "CFB Cursos", "Exemplo de ERRO");
+
+    // Caixa de informação
+//        QMessageBox::information(this, "CFB Cursos", "Informação CFB Crursos");
+
+    // Caixa de pergunta com confirmação
+//        QMessageBox::question(this, "CFB Cursos", "Você esta gostando do curso de Qt?");
+
+    // Caixa de alerta simples
+        QMessageBox::warning(this, "CFB Cursos", "Alerta simples");
 }
 
